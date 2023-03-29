4b516962ab9ac3b95fb50fc23a47bfacb7e49cb9
ravi senjaliya <shitalacademy123@gmail.com>
2023-03-29 17:24:35 +0530

    ch

---
+++ material-kit-react-main/src/pages/FinancialDB.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+// @mui
+import { Container, Stack, Typography } from '@mui/material';
+
+const FinancialDB = () => {
+  return (
+    <>
+      <Helmet>
+        <title> Dashboard: FinancialDB | Minimal UI </title>
+      </Helmet>
+
+      <Container maxWidth="xl">
+        <Typography variant="h4" sx={{ mb: 5 }}>
+          FinancialDB
+        </Typography>
+      </Container>
+    </>
+  );
+};
+
+export default FinancialDB;
---
+++ material-kit-react-main/src/pages/ProductsPage.js
@@ -0,0 +1,28 @@
+import { Helmet } from 'react-helmet-async';
+import { useState } from 'react';
+// @mui
+import { Container, Stack, Typography } from '@mui/material';
+import CartWidget from '../sections/@dashboard/products/ProductCartWidget';
+// components
+// mock
+
+// ----------------------------------------------------------------------
+
+export default function ProductsPage() {
+  return (
+    <>
+      <Helmet>
+        <title> Dashboard: Products | Minimal UI </title>
+      </Helmet>
+
+      <Container>
+        <Typography variant="h4" sx={{ mb: 5 }}>
+          Products
+        </Typography>
+
+        {/* <ProductCartWidget /> */}
+        <CartWidget />
+      </Container>
+    </>
+  );
+}
