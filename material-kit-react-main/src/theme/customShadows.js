98b668a0296974ac8d91a8b7546dab339df2bfea
Rajan Kathiriya <rajankathiriya99@gmail.com>
2023-03-29 17:56:55 +0530

    hy

--- material-kit-react-main/src/theme/customShadows.js
+++ material-kit-react-main/src/theme/customShadows.js
@@ -8,6 +8,7 @@ import palette from './palette';
 const color = palette.grey[500];
 // helloraviu
 // Rajan
+// RK
 export default function customShadows() {
   const transparent = alpha(color, 0.16);
   return {
