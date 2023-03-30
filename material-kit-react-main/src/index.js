7db287e9adf81d91cdec6ac2239052ee900f8c4e
Rajan Kathiriya <rajankathiriya99@gmail.com>
2023-03-30 16:54:34 +0530

    gh

--- material-kit-react-main/src/index.js
+++ material-kit-react-main/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
-import 'react-bootstrap'
+// import 'react-bootstrap'
 
 // ----------------------------------------------------------------------
 
